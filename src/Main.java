4bab72d395766de306cbbaf4773d73a7e2f1e342
vrnkv <vrnkv2014@yandex.ru>
2023-08-14 16:45:25 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ваше количество бонусов");
+        int price = 156891;
+        int percent = 20;
+        int bonus = price / percent;
+        System.out.println(bonus);
+    }
+}
